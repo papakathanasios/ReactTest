8acb4cd7f9272fd04504b776defd6d9b0bfaaeee
Thanos Papakostas <papakostas.athanasios@nbg.gr>
2018-05-04 10:52:28 +0300

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+"user strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); //Runs local dev server
+var open = require('gulp-open'); //Open URL in a web browser
+var browserify = require('browserify'); //Bundle JS
+var reactify = require('reactify'); //Transform React JSX to JS
+var source = require('vinyl-source-stream'); // Use Conventional text streams with Gulp
+
+var config = {
+    port: 9005,
+    devBaseUrl:'http://localhost',
+    paths: {
+        html: './src/*.html',
+        dist: './dist'
+    }
+}
+//start a local development server
+gulp.task('connect', function(){
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload:true
+    });
+});
+
+gulp.task('open', ['connect'], function(){
+    gulp.src('dist/index.html').pipe(open({uri: config.devBaseUrl+ ':' + config.port + '/'}));
+});
+
+gulp.task('html', function(){
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function(){
+    gulp.watch(config.paths.html, ['html']);
+});
+
+gulp.task('default',['html','open','watch']);
